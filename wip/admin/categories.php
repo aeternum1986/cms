acee1aaa9305b1a68e4aaa636e02fc9265cf3869
Dan Jones <bacafe@fastmail.com.au>
2020-09-02 15:52:32 +0800

    test

---
+++ wip/admin/categories.php
@@ -0,0 +1,78 @@
+<?php include "includes/header.php";
+include "includes/functions.php"; ?>
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/navigation.php";?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Subheading</small>
+                        </h1>
+                        <div class="col-xs-6">
+
+                            <?php
+                            insert_categories();
+                            ?>
+
+                            <form action="" method="post">
+                            
+                            <div class="form-group">
+                                <label for="cat_title">Add Category</label>
+                                <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                            
+                            </form>
+
+
+                            
+                            
+                            <?php if(isset($_GET['edit']))
+                            {
+                                include "includes/update_categories.php";
+                            }
+                            ?>
+                            
+                            
+                        </div>
+
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php deleteCategories(); findAllCategories();?>
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<?php include 'includes/footer.php'?>
---
+++ wip/admin/includes/functions.php
@@ -0,0 +1,63 @@
+<?php
+
+
+function insert_categories() {
+    global $connection;
+    if(isset($_POST['submit'])) {
+        $cat_title = $_POST['cat_title'];
+            if($cat_title == "" or empty($cat_title)) {
+                echo "<p class='alert alert-danger'>Field should not be empty</p>";
+            }
+            else {
+
+            $query = "INSERT INTO categories(cat_title) VALUE('{$cat_title}')";
+            $create_category_query = mysqli_query($connection, $query);
+            echo "<p class='alert alert-success'>Success!</p>";
+            
+            if(!$create_category_query) {
+
+                die("Query unsuccessful" . mysqli_error($connection));
+
+            }
+            }
+
+    }
+}
+
+function findAllCategories() {
+    //find all categories
+    global $connection;
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+                            
+
+    while($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];        
+        $cat_title = $row['cat_title'];
+        echo "<tr>";  
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+        echo "</tr>";
+    }
+}
+
+function deleteCategories() {
+    global $connection;
+    if(isset($_GET['delete'])) {
+
+        $the_cat_id = $_GET['delete'];
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+        $delete_query = mysqli_query($connection, $query);
+
+        if(!$delete_query) {
+            echo "<p class='alert alert-danger'>Error</p>";
+        } else {
+        echo "<p class='alert alert-success'>Success!</p>";
+        header("Location: categories.php");
+       }
+    }
+}
+
+?>
